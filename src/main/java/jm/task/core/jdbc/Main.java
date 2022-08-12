06db3d4c39d3fa3de833301a7eea9877e5b9795c
devourmealive <foxmanlisichka@yandex.ru>
2022-08-12 19:11:36 +0300

    updated Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,9 +1,13 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -11,13 +15,45 @@ public class Main {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "admin";
 
+    private static final String[] names = {"Alexey", "Andy", "Paj", "Hideo"};
+    private static final String[] lastNames = {"Kuznetsov", "Walker", "Pajero", "Takayama"};
+    private static final byte[] ages = {32, 19, 98, 60};
+
+    private static final String INSERT_NEW = "INSERT INTO users(name, last_name, age) VALUES (?, ?, ?)";
+
     public static void main(String[] args) {
 
-        try {
-            Connection connection = Util.getConnection(URL, USERNAME, PASSWORD);
+        ArrayList<User> users = createUsersList(names, lastNames, ages);
+
+        try (Connection connection = Util.getConnection(URL, USERNAME, PASSWORD); PreparedStatement preparedStatement = connection.prepareStatement(INSERT_NEW)) {
+            addUsersToTable(users, preparedStatement);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
+    }
+
+    public static void addUsersToTable(ArrayList<User> users, PreparedStatement preparedStatement) {
+        users.forEach(x -> {
+            try {
+                preparedStatement.setString(1, x.getName());
+                preparedStatement.setString(2, x.getLastName());
+                preparedStatement.setInt(3, x.getAge());
+
+                preparedStatement.execute();
+
+                System.out.printf("User с именем - %s добавлен в базу данных", x.getName());
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    public static ArrayList<User> createUsersList(String[] names, String[] lastNames, byte[] ages) {
+        ArrayList<User> usersList = new ArrayList<>();
+        for (int i = 0; i < names.length; i++) {
+            usersList.add(new User(names[i], lastNames[i], ages[i]));
+        }
+        return usersList;
     }
 }
