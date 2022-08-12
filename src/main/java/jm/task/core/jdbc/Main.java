35e416097fb05150ba6e1cb356cbc196880ff275
devourmealive <foxmanlisichka@yandex.ru>
2022-08-12 18:40:51 +0300

    updated Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,23 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/task_2_db";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "admin";
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+        try {
+            Connection connection = Util.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
