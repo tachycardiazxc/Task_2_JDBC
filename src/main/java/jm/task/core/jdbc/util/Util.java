6ef44830a679ce753381a1d9fb938328318263ce
devourmealive <foxmanlisichka@yandex.ru>
2022-08-15 20:40:34 +0300

    updated util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,6 +1,11 @@
 package jm.task.core.jdbc.util;
 
 import com.mysql.cj.jdbc.Driver;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -22,15 +27,28 @@ public class Util {
         DriverManager.registerDriver(driver);
     }
 
-    public static Properties setUpHibernate() {
-        Properties properties = new Properties();
+    public static Session setUpHibernate() {
+        SessionFactory sessionFactory = null;
+        try {
+            Properties properties = new Properties();
 
-        properties.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/task_2_db");
-        properties.setProperty("dialect", "org.hibernate.dialect.MySQL8Dialect");
-        properties.setProperty("hibernate.connection.username", "root");
-        properties.setProperty("hibernate.connection.password", "admin");
+            properties.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/task_2_db");
+            properties.setProperty("dialect", "org.hibernate.dialect.MySQL8Dialect");
+            properties.setProperty("hibernate.connection.username", "root");
+            properties.setProperty("hibernate.connection.password", "admin");
+            properties.setProperty("hibernate.hbm2ddl.auto", "create");
 
-        return properties;
+            sessionFactory = new org.hibernate.cfg.Configuration()
+                    .addProperties(properties)
+                    .addAnnotatedClass(User.class)
+                    .buildSessionFactory();
+            return getSession(sessionFactory);
+        } catch (HibernateException ignored) {}
+        return null;
+    }
+
+    private static Session getSession(SessionFactory sessionFactory) throws HibernateException {
+        return sessionFactory.openSession();
     }
 
 }
