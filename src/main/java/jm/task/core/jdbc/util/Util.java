1922ba5ed1cba58a68a45f2733c9dd61db52c344
devourmealive <foxmanlisichka@yandex.ru>
2022-08-15 20:30:28 +0300

    hot fix

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -4,6 +4,7 @@ import com.mysql.cj.jdbc.Driver;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
 
@@ -19,6 +20,17 @@ public class Util {
     private static void getDriver() throws SQLException {
         Driver driver = new com.mysql.jdbc.Driver();
         DriverManager.registerDriver(driver);
+    }
+
+    public static Properties setUpHibernate() {
+        Properties properties = new Properties();
+
+        properties.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/task_2_db");
+        properties.setProperty("dialect", "org.hibernate.dialect.MySQL8Dialect");
+        properties.setProperty("hibernate.connection.username", "root");
+        properties.setProperty("hibernate.connection.password", "admin");
+
+        return properties;
     }
 
 }
